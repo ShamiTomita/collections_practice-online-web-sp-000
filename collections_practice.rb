b060fcc275bd3352fda1948569a657ab6dc35603
Shami Tomita <shamitomita@gmail.com>
2021-01-13 22:40:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -52,7 +52,7 @@ end
 array = ["hand","feet", "knee", "table"]
 def add_s(array)
   new_array = []
-  array.each_with_index.reject do |word|
+  array.each_with_index.reject {|el,i| i == 1}.each do |el,i|
   word.insert(-1, "s")
   new_array.push(word)
   end 
